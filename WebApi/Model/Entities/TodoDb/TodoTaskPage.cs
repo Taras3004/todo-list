39574bb99a290d21750d057eb12837db4a7a8ae8
Taras Serhiienko <taras.t.s11@gmail.com>
2025-10-30 18:17:59 +0200

    Task page access restricted & Entities moved to models

--- WebApi/Model/Entities/TodoDb/TodoTaskPage.cs
+++ WebApi/Model/Entities/TodoDb/TodoTaskPage.cs
@@ -2,10 +2,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebApi.Entities.TodoDb;
 
-public class TodoTaskPage : BaseEntity
+public class TodoTaskPage : BaseEntity, IUserOwnedEntity
 {
     public int TodoTaskId { get; set; }
 
     [StringLength(500)]
     public string? Description { get; set; }
+
+    [ValidateNever]
+    public string UserId { get; set; }
+
+    [ValidateNever]
+    public virtual ApplicationUser User { get; set; }
 }
